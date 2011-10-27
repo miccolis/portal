2d92a04bd393978651c22b6c1bfc65745e1f50a2
Jeff Miccolis <jeff@miccolis.net>
2011-10-26 21:27:47 -0400

    Adding first work on a CKAN like CouchApp.

---
+++ app.js
@@ -0,0 +1,60 @@
+var path = require('path'),
+    couchapp = require('couchapp'),
+    _ = require('underscore');
+
+ddoc = 
+  { _id:'_design/app'
+  , rewrites : 
+    [ {from:"/", to:'index.html'}
+    , {from:"/api", to:'../../'}
+    , {from:"/api/*", to:'../../*'}
+    , {from:"/*", to:'*'}
+    ]
+  }
+  ;
+
+ddoc.views = {};
+
+ddoc.views.tags = {
+    map: function(doc) {
+        if(doc._id.match(/^dataset\//)) {
+            doc.tags.forEach(function(d){
+                emit(d, null);
+        });
+       }
+    },
+    reduce: "_count"
+};
+
+ddoc.validate_doc_update = function (newDoc, oldDoc, userCtx) {   
+  if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
+    throw "Only admin can delete documents on this database.";
+  } 
+}
+
+// Load normal attachments.
+couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
+
+// Load templates as a single attachment.
+couchapp.loadAttachments(ddoc, path.join(__dirname, 'templates'), {
+  operators: [function(f, data) {
+      if (path.extname(f) === '._') {
+          var t= ';;'
+          t+= 'templates.'+ path.basename(f, '._') + '=';
+          t+= _.template(data.toString('utf8'));
+          return new Buffer(t); 
+      }
+      return data; 
+  }],
+  aggregator: function(files) {
+      var templates = 'var templates = templates || {};\n',
+          aggregated = [];
+      files.forEach(function(f) {
+          templates += f.data.toString('utf8');
+      });
+      aggregated.push({data: new Buffer(templates), name: 'templates.js', mime: 'text/javascript'});
+      return aggregated;
+  }
+});
+
+module.exports = ddoc;
---
+++ attachments/site.js
@@ -0,0 +1,249 @@
+var request = function (options, callback) {
+  options.success = function (obj) {
+    callback(null, obj);
+  }
+  options.error = function (err) {
+    if (err) callback(err);
+    else callback(true);
+  }
+  if (options.data && typeof options.data == 'object') {
+    options.data = JSON.stringify(options.data)
+  }
+  if (!options.dataType) options.processData = false;
+  if (!options.dataType) options.contentType = 'application/json';
+  if (!options.dataType) options.dataType = 'json';
+  $.ajax(options)
+}
+
+$.expr[":"].exactly = function(obj, index, meta, stack){ 
+  return ($(obj).text() == meta[3])
+}
+
+var param = function( a ) {
+  // Query param builder from jQuery, had to copy out to remove conversion of spaces to +
+  // This is important when converting datastructures to querystrings to send to CouchDB.
+  var s = [];
+  if ( jQuery.isArray(a) || a.jquery ) {
+          jQuery.each( a, function() { add( this.name, this.value ); });		
+  } else { 
+    for ( var prefix in a ) { buildParams( prefix, a[prefix] ); }
+  }
+  return s.join("&");
+	function buildParams( prefix, obj ) {
+		if ( jQuery.isArray(obj) ) {
+			jQuery.each( obj, function( i, v ) {
+				if (  /\[\]$/.test( prefix ) ) { add( prefix, v );
+				} else { buildParams( prefix + "[" + ( typeof v === "object" || jQuery.isArray(v) ? i : "") +"]", v )}
+			});				
+		} else if (  obj != null && typeof obj === "object" ) {
+			jQuery.each( obj, function( k, v ) { buildParams( prefix + "[" + k + "]", v ); });				
+		} else { add( prefix, obj ); }
+	}
+	function add( key, value ) {
+		value = jQuery.isFunction(value) ? value() : value;
+		s[ s.length ] = encodeURIComponent(key) + "=" + encodeURIComponent(value);
+	}
+}
+
+// Determine the name of the CouchDB we're working with.
+var rootPath = /(.+)_design/.exec(location.pathname);
+if (rootPath.length < 2) {
+    console.error("Couldn't determine database name");
+}
+rootPath = rootPath[1];
+
+// Begin Backbone setup.
+var models = {};
+models.Package = Backbone.Model.extend({
+    url: function() {
+        return rootPath + this.id;
+    },
+    schema: {
+        name: 'package',
+        properties: {
+            'id': {
+                type: 'string',
+                description: 'unique id',
+                required: 'true'
+            },
+            'name': {
+                type: 'string',
+                description: 'unique name that is used in urls and for identification',
+                required: 'true'
+            },
+            'title': {
+                type: 'string',
+                description: 'short title for dataset',
+                required: 'true',
+                format: 'text'
+            },
+            'url': {
+                type: 'string',
+                description: 'home page for this dataset',
+                required: 'true',
+                format: 'uri'
+            },
+            'author': {
+                type: 'string',
+                description: 'original creator of the dataset',
+                required: 'true',
+                format: 'text'
+            },
+            'author_email': {
+                type: 'string',
+                description: 'email for original creator of the dataset',
+                required: 'true',
+                format: 'email'
+            },
+            'maintainer': {
+                type: 'string',
+                description: 'current maintainer or publisher of the dataset',
+                required: 'true',
+                format: 'text'
+            },
+            'maintainer_email': {
+                type: 'string',
+                description: 'email for current maintainer or publisher of the dataset',
+                required: 'true',
+                format: 'email'
+            },
+            'license': {
+                type: 'string',
+                description: 'license under which the dataset is made available',
+                required: 'true',
+                format: 'text'
+            },
+            'version': {
+                type: 'string',
+                description: 'dataset version',
+                required: 'true',
+                format: 'text'
+            },
+            'notes': {
+                type: 'string',
+                description: 'description and other information about the dataset',
+                required: 'true',
+                format: 'text'
+            },
+            'tags': { 
+                type: 'string',
+                description: 'arbitrary textual tags for the dataset',
+                required: 'true'
+            },
+            'resources': {
+                type: 'string',
+                description: 'list of Resources',
+                required: 'true'
+            },
+            'groups': {
+                type: 'string',
+                description: 'list of Groups this dataset is a member of',
+                required: 'true'
+            }
+        }
+    }
+});
+
+models.Packages = Backbone.Collection.extend({
+    model: models.Package,
+    url: rootPath + '/_all_docs?include_docs=true',
+    parse: function(resp) {
+        return _(resp.rows).pluck('doc');
+    }
+});
+
+models.Resource = Backbone.Model.extend({
+    url: function() {
+        return rootPath + this.id;
+    },
+    schema: {
+        name: 'package',
+        properties: {
+            url: {
+                type: 'string',
+                description: 'The url points to the location online where the content of that resource can be found. For a file this would be the location online of that file (or more generally a url which yields the bitstream representing the contents of that file. For an API this would be the endpoint for the api.',
+                required: 'true'
+            },
+            name: {
+                type: 'string',
+                description: 'a name for this resource (could be used in a ckan url)'
+            },
+            description: {
+                type: 'string',
+                description: 'A brief description (one sentence) of the Resource. Longer descriptions can go in notes field of the associated Data Package.',
+            },
+            type: {
+                type: 'string',
+                description: 'the type of the resource. One of: file | api | service | listing'
+            },
+            file: {
+                description: '- a file (GET of this url should yield a bitstream)\n api - an API\nservice (?) - an online service such as google docs\nlisting (?) - a listing or index resource (a page listing of other resources). It is common, at present, to find projects where the data is in lots of files with these files listed on an index page. Rather than attempt to create a resource entry for each file we have adopted the convention of creating a resource for the relevant listing page.'
+            },
+            format: {
+                description: 'human created format string with possible nesting e.g. zip:csv. See below for details of the format field.'
+            },
+            mimetype: {
+                description: 'standard mimetype (e.g. for zipped csv would be application/zip)'
+            },
+            mimetype_inner: {
+                description: 'mimetype of innermost object (so for example would be text/csv)'
+            },
+            size: {
+                description: 'size of the resource (content length). Usually only relevant for resources of type file.'
+            },
+            last_modified: {
+                description: 'the date when this resource\'s data was last modified (NB: not the date when the metadata was modified).'
+            },
+            hash: {
+                description: 'md5 or sha-1 hash'
+            }
+        }
+    }
+});
+
+var views = {};
+
+views.Package = Backbone.View.extend();
+
+views.Catalog = Backbone.View.extend({
+    initialize: function() {
+        _.bindAll(this, 'render');
+        var view = this;
+        this.collection.bind('all', function() { view.render(); });
+    },
+    render: function() {
+        debugger;
+        return this;
+    }
+});
+
+
+var App = Backbone.Router.extend({
+    routes: {
+        '': 'home',
+        'catalog/:id': 'package'
+    },
+    home: function() {
+        var collection = new models.Packages();
+        new views.Catalog({
+            el: $('#main'),
+            collection: collection 
+        });
+        collection.fetch();
+    },
+    package: function(id) {
+        var model = new models.Package({id:id});
+        new views.Package({
+            el: $('#main'),
+            model: model
+        });
+        model.fetch();
+    },
+});
+
+$(function () { 
+    new App();
+    Backbone.history.start({
+        root: location.pathname
+    });
+});
